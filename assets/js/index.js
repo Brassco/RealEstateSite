2f0a69b2b3d21bf9cc6191ec0439ff217fc932bf
Brassco <tonny.brassco@gmail.com>
2022-06-09 08:05:42 +0300

    Completed Popular section

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+/* ========== CHANGE BACKGROUND HEADER ==========*/
+function scrollHeader() {
+    const header = document.getElementById('header');
+
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header')
+    } else {
+        header.classList.remove('scroll-header')
+    }
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+/* ========== SWIPER POPULAR ==========*/
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  /* ==========  ==========*/
